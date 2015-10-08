4b3eb4d7b19c68bfe0557ea2e8a9cf52eb1e59aa
Paul Clark <pdclark@pdclark.com>
2015-10-07 21:58:25 -0700

    Icons, images, etc.

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ jQuery(document).ready( function( $ ) {
 		if ( $menuToggle.hasClass( 'active' ) ) {
 			$menuToggle.removeClass('active');
 			$('body').removeClass('menu-open');
-		} else {
+		} else if ( $(this).hasClass('js-menu') ) {
 			$menuToggle.addClass('active');
 			$('body').addClass('menu-open');
 		}
