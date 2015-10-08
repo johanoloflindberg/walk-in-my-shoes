e90f351af17f405db09858130cf4da789ec1283d
Paul Clark <pdclark@pdclark.com>
2015-10-08 02:13:51 -0700

    Animate feet

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,9 @@ jQuery(document).ready( function( $ ) {
 
 	var init = function () {
 		$menuToggles.click( toggleMenu );
+
+		Flowtime.addEventListener("flowtimenavigation", onNavigation, false);
+		onNavigation();
 	}
 
 	var toggleMenu = function() {
@@ -22,6 +25,46 @@ jQuery(document).ready( function( $ ) {
 			
 		}
 	};
+
+
+	// Flowtime.onNavigation(onNavigation);
+	var onNavigation = function (e) {
+		animateFeet();
+
+		// console.log(cacheTitle + ' > ' + document.title.replace("Flowtime.js | ", ""));
+		//console.log('section', e.section, 'sectionIndex', e.sectionIndex);
+		//console.log('page', e.page, 'pageIndex', e.);
+		//console.log('pastSectionIndex', e.pastSectionIndex, 'pastPageIndex', e.pastPageIndex);
+		//console.log('prevSection', e.prevSection);
+		//console.log('nextSection', e.nextSection);
+		//console.log('prevPage', e.prevPage);
+		//console.log('nextPage', e.nextPage);
+		//console.log('fragment', e.fragment, + 'fragmentIndex', e.fragmentIndex);
+		//console.log("isOverview", e.isOverview);
+		//console.log('progress:', e.progress, 'total:', e.total);
+		// var value = Math.round(e.progress * 100 / e.total);
+		// console.log('Completion: ' + value + '%');
+
+		console.log( e );
+	}
+
+	var animateFeet = function ( e ) {
+		if (
+			   -1 !== location.hash.indexOf( 'amanda' )
+			|| -1 !== location.hash.indexOf( 'dave' )
+			|| -1 !== location.hash.indexOf( 'james' )
+		) {
+			$('#feet').show();
+		}else {
+			$('#feet').hide();
+		}
+
+		$('#feet').addClass('moving');
+
+		setTimeout( function(){
+			$('#feet').removeClass('moving');
+		}, 2000 );
+	}
 
 	init();
 
